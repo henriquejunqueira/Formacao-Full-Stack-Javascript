f29bcf54d7b9be89435e7076db20fa6daa73149c
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-05 10:49:05 -0300

    Statements

---
+++ modulo19-js-basico/aula04/script.js
@@ -0,0 +1,31 @@
+// comentário de uma linha
+
+/*
+   comentário de múltiplas linhas
+*/
+
+/*
+   Um programa de computador é uma lista de "instruções" a serem "executadas" por um computador
+   Um programa JavaScript é uma lista de instruções de programação.
+   Valores, operadores, expressões, palavras-chave e comentários
+
+   break
+   continue
+   debugger
+   do ... while
+   for
+   function
+   if ... 
+   return
+   switch
+   try ...
+   var
+   let
+   const
+   document
+*/
+
+console.log('Hello World!');
+console.log('Hello World 2!');
+
+document.querySelector('#elemento').src = 'imagem.jpg';
