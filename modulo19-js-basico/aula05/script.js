9ef3045e8b01bd541a43688157af56dcf3e3471c
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-05 11:15:10 -0300

    var, let, const

---
+++ modulo19-js-basico/aula05/script.js
@@ -0,0 +1,15 @@
+// var, let, const
+
+// string
+// var nome = 'Henrique';
+
+// number
+// var idade = 35;
+
+// let nome = 'Henrique';
+
+const nome = 'Henrique';
+
+// nome = 'João';
+
+console.log(nome);
